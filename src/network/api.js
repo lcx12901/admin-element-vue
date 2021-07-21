6b7cb0d61e2ef54b91f8dabb69703f676a4ff1de
lcx12901 <1367372276@qq.com>
2021-07-21 15:41:33 +0800

    更新api

--- src/network/api.js
+++ src/network/api.js
@@ -12,16 +12,16 @@ export const reqMenus = () => request({url:'/menus'});
 export const reqUsers = (params) => request({url: '/users', params});
 
 // 修改用户状态  users/:uId/state/:type
-export const reqChangeState = (uId, type) => request({url: `users/${uId}/state/${type}`, method: 'put'});
+export const reqChangeState = (uId, type) => request({url: `/users/${uId}/state/${type}`, method: 'put'});
 
 // 添加用户
 export const reqAddUser = (data) => request({url: '/users', method: 'post', data});
 
 // 编辑用户提交
-export const reqEditUser = (id,data) => request({url: `users/${id}`, method: 'put', data});
+export const reqEditUser = (id,data) => request({url: `/users/${id}`, method: 'put', data});
 
 // 根据 ID 查询用户信息
-export const reqUser = (id) => request({url: `users/${id}`});
+export const reqUser = (id) => request({url: `/users/${id}`});
 
 // 删除用户
 export const reqDelUser = (id) => request({url: `/users/${id}`, method: 'delete'});
@@ -30,4 +30,31 @@ export const reqDelUser = (id) => request({url: `/users/${id}`, method: 'delete'
 export const reqRoles = () => request({url: '/roles', method: 'get'});
 
 // 分配用户角色
-export const reqSetUserRole = (id,rid) => request({url: `users/${id}/role`, method: 'put', data: {rid}})
+export const reqSetUserRole = (id,rid) => request({url: `users/${id}/role`, method: 'put', data: {rid}});
+
+// 商品列表数据
+export const reqGoods = (params) => request({url: '/goods', params});
+
+// 根据id查询商品
+export const reqGood = (id) => request({url: `/goods/${id}`});
+
+// 获取商品分类
+export const reqCategories = (params) => request({url: '/categories', params});
+
+// 获取商品分类对应参数  categories/:id/attributes
+export const reqAttributes = (id, sel) => request({url: `/categories/${id}/attributes`, params: {sel}});
+
+// 添加动态参数或者静态属性
+export const reqAddAttributes = (data) => request({url: `/categories/${data.id}/attributes`, method: 'post', data});
+
+// 编辑参数或属性
+export const reqEditAttributes = (data) => request({url: `categories/${data.id}/attributes/${data.attrId}`, method: 'put', data});
+
+// 删除参数
+export const reqDeleteAttributes = (id, attrId) => request({url: `categories/${id}/attributes/${attrId}`, method: 'delete'});
+
+// 添加商品
+export const reqAddGood = (data) => request({url: '/goods', method: 'post', data});
+
+// 删除商品
+export const reqDeleteGood = (id) => request({url: `/goods/${id}`, method: 'delete'});
