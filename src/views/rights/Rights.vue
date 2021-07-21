a3e1efd1f5fc02ef544ba7faba984f8047a8b9b1
lincx <1367372276@qq.com>
2021-07-21 23:59:28 +0800

    权限列表页面

--- src/views/rights/Rights.vue
+++ src/views/rights/Rights.vue
@@ -1,10 +1,58 @@
 <template>
-    <div class="rights"></div>
+    <div class="rights">
+        <el-card class="container" shadow="hover">
+            <el-table
+                :data="rightsList"
+                stripe
+                border
+                style="width: 100%"
+            >
+                <el-table-column type="index">
+                </el-table-column>
+                <el-table-column
+                    property="authName"
+                    label="权限名称"
+                >
+                </el-table-column>
+                <el-table-column
+                    property="path"
+                    label="路径"
+                >
+                </el-table-column>
+                <el-table-column
+                    label="权限等级"
+                >
+                    <template slot-scope="{row}">
+                        <el-tag size="mini" v-if="row.level == '0'">一级</el-tag>
+                        <el-tag size="mini" type="success" v-else-if="row.level == '1'">二级</el-tag>
+                        <el-tag size="mini" type="warning" v-else>三级</el-tag>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-card>
+    </div>
 </template>
 
 <script>
+import {reqRights} from 'network/api.js'
 export default {
-    name: 'Rights'
+    name: 'Rights',
+    data () {
+        return {
+            rightsList: []
+        }
+    },
+
+    created () {
+        this.getRights()
+    },
+
+    methods: {
+        async getRights () {
+            const {data} = await reqRights('list')
+            this.rightsList = data
+        }
+    }
 }
 </script>
 
