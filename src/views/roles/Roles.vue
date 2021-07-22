039710b19ac05f47eda9e9bab6343d60b38743b5
lincx <1367372276@qq.com>
2021-07-23 00:04:04 +0800

    角色列表页面

--- src/views/roles/Roles.vue
+++ src/views/roles/Roles.vue
@@ -1,10 +1,58 @@
 <template>
-    <div class="roles"></div>
+    <div class="roles">
+        <el-card class="container" shadow="hover">
+            <el-button type="primary" plain>添加角色</el-button>
+            <el-table
+                :data="rolesList"
+                stripe
+                border
+                style="width: 100%">
+                <el-table-column type="expand">
+                </el-table-column>
+                <el-table-column
+                    type="index"
+                    label="#"
+                />
+                <el-table-column
+                    label="角色名称"
+                    prop="roleName"
+                />
+                <el-table-column
+                    label="角色描述"
+                    prop="roleDesc"
+                />
+                <el-table-column
+                    label="操作"
+                >
+                    <template>
+                        <el-button size="small" type="primary" icon="el-icon-edit">编辑</el-button>
+                        <el-button size="small" type="danger" icon="el-icon-delete">删除</el-button>
+                        <el-button size="small" type="warning" icon="el-icon-setting">分配权限</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-card>
+    </div>
 </template>
 
 <script>
+import {reqRoles} from 'network/api.js'
 export default {
-    name: 'Roles'
+    name: 'Roles',
+    data () {
+        return {
+            rolesList: []
+        }
+    },
+    created () {
+        this.getRoles()
+    },
+    methods: {
+        async getRoles () {
+            const {data} = await reqRoles()
+            this.rolesList = data
+        }
+    }
 }
 </script>
 
